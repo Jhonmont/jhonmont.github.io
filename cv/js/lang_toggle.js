1b7ce5d7cfcab15b25e1eba556a6a8a05aa925e1
Jhonatan Montilla <jhonmont@gmail.com>
2023-09-06 20:39:18 -0300

    CV site structure changes
    Generate new site structure for cv in bim and bi experiences.

--- cv/js/lang_toggle.js
+++ cv/js/lang_toggle.js
@@ -4,7 +4,7 @@ const flagsElements = document.getElementById("flags");
 const textsToChange = document.querySelectorAll("[data-section]");
 
 const changeLanguage = async language => {
-    const requestJson = await fetch(`./lang/${language}.json`);
+    const requestJson = await fetch(`../lang/${language}.json`);
     const texts = await requestJson.json();
     
     for (const textToChange of textsToChange) {
